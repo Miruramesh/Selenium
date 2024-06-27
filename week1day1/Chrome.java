3f43fd35e2d2b81b9d40de90ab239eeec0457fba
Miruramesh <Maddyrush0325@gmail.com>
2024-06-27 14:56:02 +0530

    Add files via upload
    Week 1

---
+++ week1day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float versionType = 91.0f;
+		String developerType = "Google";
+		boolean betaType = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		System.out.println("The Version Type is "+versionType);
+		System.out.println("The Developer Value is "+developerType);
+		System.out.println("The Beta Type is "+betaType);
+		System.out.println("The Release Year is "+releaseYear);
+		System.out.println("The Shortcut Key value is "+shortcutKey);
+
+	}
+
+}
